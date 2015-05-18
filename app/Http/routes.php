af3599a5a566c92cd337ff6d94160cab5e2912ce
Tom Rochette <roctom@gmail.com>
2015-05-17 20:33:23 -0400

    Move data entry route from / to /logs. Add a /logs/bulk route to allow bulk entry of data.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,15 +2,31 @@
 
 use Carbon\Carbon;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 
 Route::get('/', function() {
-	$data = DB::table('log')->select('key')->groupBy('key')->get();
-	return Arr::pluck($data, 'key');
+	return View::make('home');
 });
 
-Route::post('/', function() {
-	$timestamp = Carbon::parse(Input::get('timestamp'));
-	$data = Input::except(['timestamp']);
+Route::get('logs', function() {
+	return getKeys();
+});
+
+function getKeys() {
+	$data = DB::table('log')->select('key')->groupBy('key')->get();
+	return Arr::pluck($data, 'key');
+}
+
+// Accepts
+// {
+// 	"_timestamp": "now",
+// 	"a": 6,
+// 	"b": 2,
+// 	"c": 12
+// }
+Route::post('logs', function() {
+	$timestamp = Carbon::parse(Input::get('_timestamp', 'now'));
+	$data = Input::except(['_timestamp']);
 
 	$insertedData = [];
 	foreach ($data as $key => $value) {
@@ -26,7 +42,43 @@ Route::post('/', function() {
 	return ['timestamp' => $timestamp->timestamp];
 });
 
-Route::get('/{key}', function($key) {
+// Accepts
+// [
+// 	{
+// 		"_timestamp": "now",
+// 		"a": 6,
+// 		"b": 2,
+// 		"c": 12
+// 	},
+// 	{
+// 		"_timestamp": "tomorrow",
+// 		"d": 6,
+// 		"e": 2,
+// 		"f": 12
+// 	}
+// ]
+Route::post('logs/bulk', function() {
+	$data = Input::all();
+
+	$insertedData = [];
+	foreach ($data as $metricData) {
+		$timestamp = Carbon::parse(Arr::get($metricData, '_timestamp', 'now'));
+		$metrics = Arr::except($metricData, ['_timestamp']);
+		foreach ($metrics as $key => $value) {
+			$insertedData[] = [
+				'timestamp' => $timestamp->timestamp,
+				'key' => (string)$key,
+				'value' => (double)$value,
+			];
+		}
+	}
+
+	DB::table('log')->insert($insertedData);
+
+	return ['status' => 'ok', 'inserted' => count($insertedData)];
+});
+
+Route::get('logs/{key}', function($key) {
 	$from = Input::get('from');
 	$from = $from ? Carbon::parse($from) : null;
 	$to = Input::get('to');
@@ -46,8 +98,7 @@ Route::get('/{key}', function($key) {
 	}, $data);
 });
 
-function whereFromTo($query, $from, $to)
-{
+function whereFromTo($query, $from, $to) {
 	if ($from && $to) {
 		return $query->whereBetween('log.timestamp', [$from->timestamp, $to->timestamp]);
 	} else if ($from) {
@@ -56,3 +107,28 @@ function whereFromTo($query, $from, $to)
 		return $query->where('log.timestamp', '<', $to->timestamp);
 	}
 }
+
+Route::get('all', function() {
+	$data = DB::table('log')
+		->select('log.*')
+		->groupBy('timestamp')
+		->groupBy('key')
+		->get();
+
+	$data = (new Collection($data))->groupBy('key');
+
+	$output = [];
+
+	foreach ($data as $group => $items) {
+		$entry = [
+			'name' => $group,
+			'data' => [],
+		];
+		foreach ($items as $item) {
+			$entry['data'][] = [(int)$item->timestamp*1000, (float)$item->value];
+		}
+		$output[] = $entry;
+	}
+
+	return Response::json($output);
+});
